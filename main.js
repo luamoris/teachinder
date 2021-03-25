aa6e1d02d9e2a77d681b39969a299fa513575a74
Iosif <yaiosifua@gmail.com>
2021-03-25 19:27:36 +0200

    fix lint problem

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
 // =
-const { randomUserMock, additionalUsers } = require('./data/mock.js');
-
-
-function dataFormatting(oldUsers) {
-	const users = [];
-	let i = 0;
-	oldUsers.forEach(item => users.push(createUsers(item, i)))
-	return users;
-}
+// const { randomUserMock, additionalUsers } = require('./data/mock.js');
 
 const createUsers = (oldUser, id) => {
-	return {
+	const user = {
 		id: oldUser.id.value ? `${oldUser.id.name}${oldUser.id.value}` : `NEW${1000000000 + id}`,
 		gender: oldUser.gender,
 		title: oldUser.name.title,
@@ -37,41 +29,47 @@ const createUsers = (oldUser, id) => {
 		bg_color: '',
 		note: '',
 	};
-}
+	return user;
+};
 
+function dataFormatting(oldUsers) {
+	const users = [];
+	let i = 0;
+	oldUsers.forEach((item) => users.push(createUsers(item, i++)));
+	return users;
+}
 
 // console.log(randomUserMock.length)
 // dataFormatting(randomUserMock)
-// 	.filter((element, index, arr) => arr.findIndex(el => (el.id === element.id)) === index)
-// 	.forEach((item, index) => console.log(item.phone));
-
+// .filter((element, index, arr) => arr.findIndex(el => (el.id === element.id)) === index)
+// .forEach((item, index) => console.log(item.phone));
 
 /* =======================================================
 	2. Validation
 ========================================================== */
 
 // Validation string
-const validString = str => /^[\p{Lu}]{1}[\p{Ll}]+$/u.test(str);
+const validString = (str) => /^[\p{Lu}]{1}[\p{Ll}]+$/u.test(str);
 
 // Validating an array of strings
-const validArrStrings = strs => strs.length === strs.filter(element => validString(element)).length;
+const validArrStrings = (strs) => strs.length
+	=== strs.filter((element) => validString(element)).length;
 
 // Validating a numeric integer value
-const validIsInteger = num => (typeof(num) === 'number') && ((num ^ 0) === num) && num > 0;
+const validIsInteger = (num) => (typeof (num) === 'number') && ((num ^ 0) === num) && num > 0;
 
 // Validation e-mail
-const validIsEmail = email => /^[^\W\d_]\w+@\w+\.\w+(\.\w+)?$/.test(email);
+const validIsEmail = (email) => /^[^\W\d_]\w+@\w+\.\w+(\.\w+)?$/.test(email);
 
 // Validation phone number
-const validIsPhoneNumber = number => /(\+)?([- _():=+]?\d[- _():=+]?){8,14}/.test(number);
-
+const validIsPhoneNumber = (number) => /(\+)?([- _():=+]?\d[- _():=+]?){8,14}/.test(number);
 
 // Object validation
 function ValidationUser(user) {
-	return validArrStrings([user.full_name, user.gender, user.note, user.state, user.city, user.country])
-		&& validIsInteger(user.age) && validIsPhoneNumber(user.number) && validIsEmail(user.email);
+	return validArrStrings([
+		user.full_name, user.gender, user.note, user.state, user.city, user.country,
+	]) && validIsInteger(user.age) && validIsPhoneNumber(user.number) && validIsEmail(user.email);
 }
-
 
 /* =======================================================
 	3. Filtration
@@ -79,10 +77,11 @@ function ValidationUser(user) {
 
 // Users filtration
 function FilterUsers(users, opts) {
-	return users.filter((el) => (opts.country ? el.country === opts.country : true) && (opts.age ? el.age === opts.age : true)
-		&& (opts.gender ? el.gender === opts.gender : true) && (opts.favorite ? el.favorite === opts.favorite : true));
+	return users.filter((el) => (opts.country ? el.country === opts.country : true)
+		&& (opts.age ? el.age === opts.age : true)
+		&& (opts.gender ? el.gender === opts.gender : true)
+		&& (opts.favorite ? el.favorite === opts.favorite : true));
 }
-
 
 /* =======================================================
 	4. Sorting
@@ -90,41 +89,41 @@ function FilterUsers(users, opts) {
 
 // Comparing two values of the same type
 const anyCompare = (elm1, elm2) => {
-	if(typeof(elm1) !== typeof(elm2)) {
+	if (typeof (elm1) !== typeof (elm2)) {
 		throw Error("It is not possible to compare two values of different types.");
 	}
 	return (elm1 > elm2) ? 1 : ((elm2 > elm1) ? -1 : 0);
-}
+};
 
 // Comparing two dates
 const dateCompare = (date1, date2) => {
 	const reg = /^\d{1,4}-(0(?=[1-9])|1(?=[0-2]))[0-9]-(0(?=[1-9])|(1|2)(?=[0-9])|3(?=[0-1]))[0-9]T((0|1|2)(?=[0-9]))[0-9]:((0|1|2|3|4|5)(?=[0-9]))[0-9]:((0|1|2|3|4|5)(?=[0-9]))[0-9]\.[0-9]{1,3}Z$/;
-	if ((typeof(date1) !== 'string' || typeof(date2) !== 'string') && (reg.test(date1) || reg.test(date2))) {
+	if ((typeof (date1) !== 'string' || typeof (date2) !== 'string') && (reg.test(date1) || reg.test(date2))) {
 		throw Error("Date format is incorrect.");
 	}
 	const d1 = Date.parse(date1);
 	const d2 = Date.parse(date2);
-	return d1 > 0 ? (d2 > 0 ? ((d1 > d2) ? 1 : ((d2 > d1) ? -1 : 0)) : 1) : (d2 > 0 ? -1 : ((d1 > d2) ? 1 : ((d2 > d1) ? -1 : 0)));
-}
+	return d1 > 0 ? (d2 > 0 ? ((d1 > d2) ? 1 : ((d2 > d1) ? -1 : 0)) : 1)
+		: (d2 > 0 ? -1 : ((d1 > d2) ? 1 : ((d2 > d1) ? -1 : 0)));
+};
 
 // Validating options for sorting
 const validTemplateSort = (opts) => {
-	for(const item of opts) {
-		if (!item.field || typeof(item.field) !== 'string' || (item.type !== 'any' && item.type !== 'date') 
+	for (const item of opts) {
+		if (!item.field || typeof (item.field) !== 'string' || (item.type !== 'any' && item.type !== 'date')
 			|| (item.method !== 'ASC' && item.method !== 'DESC')) { throw Error("Template values are incorrect."); }
 		item.type === 'any' ? (item.sort = anyCompare) : (item.sort = dateCompare);
 	}
 	return opts;
-}
+};
 
 // Sorting an array of objects
 function SortUsers(users, opts) {
 	const cust = validTemplateSort(opts);
 	const compareMethod = (method, func, arg1, arg2) => method === 'ASC' ? func(arg1, arg2) : func(arg2, arg1);
 	const compare = (index, a, b, field) => cust[index] ? compareMethod(cust[index].method, cust[index].sort, a[`${field}`], b[`${field}`]) : 0;
-	return users.sort((a, b) => {
-		return compare(0, a, b, cust[0].field) || compare(1, a, b, cust[1].field) || compare(2, a, b, cust[2].field) || compare(3, a, b, cust[3].field);
-	});
+	return users.sort((a, b) => compare(0, a, b, cust[0].field) || compare(1, a, b, cust[1].field)
+		|| compare(2, a, b, cust[2].field) || compare(3, a, b, cust[3].field));
 }
 
 /*
@@ -140,15 +139,20 @@ function SortUsers(users, opts) {
 
 */
 
-
 /* ======================================================= */
 
 module.exports = {
+
+	// Formation
+	dataFormatting,
+
 	// Validation
 	validString,
 	validIsInteger,
 	validIsEmail,
 	validIsPhoneNumber,
+	ValidationUser,
+
 	// Filtration
 	FilterUsers,
 
@@ -156,4 +160,5 @@ module.exports = {
 	anyCompare,
 	dateCompare,
 	validTemplateSort,
+	SortUsers,
 };
